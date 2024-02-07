8c6212b347b0bbbc15e6496888be89fe2124a831
MarcusMellberg <marcus.mellberg@gmail.com>
2024-02-07 16:09:38 +0100

    More small fixes.

--- FribergCarRentalsRazorPages/Data/Customer.cs
+++ FribergCarRentalsRazorPages/Data/Customer.cs
@@ -1,20 +1,29 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace FribergCarRentalsRazorPages.Data
 {
     public class Customer
     {
+        [DisplayName("Customer Id")]
         public int Id { get; set; }
+
+        [DisplayName("Firstname")]
         [Required(ErrorMessage = "Firstname is required")]
         public string FirstName { get; set; }
+
+        [DisplayName("Lastname")]
         [Required(ErrorMessage = "Lastname is required")]
         public string LastName { get; set; }
+
         [Required(ErrorMessage = "Email Address is required")]
         [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
+
         [Required(ErrorMessage = "Password is required")]
         [MinLength(5, ErrorMessage = "Password needs to be atleast 5 characters")]
         public string Password { get; set; }
+
         public virtual List<Booking>? Bookings { get; set; }
     }
 }
